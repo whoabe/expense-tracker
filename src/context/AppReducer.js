e620cd25dadd72ccfce3c3889896fd1a97f34f8e
Abraham Ho <whoabe@gmail.com>
2020-02-23 22:24:16 -0800

    first commit

---
+++ src/context/AppReducer.js
@@ -0,0 +1,20 @@
+// specify application state changes in response to changes in store/context
+
+export default (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          transaction => transaction.id !== action.payload
+        )
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      };
+    default:
+      return state;
+  }
+};
